3a9495642f7a8a97f6ffeafc55cabc6c3b58a7a4
sultgalu <komlosi.norbii@gmail.com>
2021-12-02 21:49:47 +0100

    create main project

---
+++ src/test/java/hu/obuda/java/warehouse/WarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.obuda.java.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
